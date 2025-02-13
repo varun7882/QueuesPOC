0ee125302a193b1ca16f49e78df4a687e14e90fe
varun7882 <varunparas.srivastava8@gmail.com>
2025-02-13 22:37:06 +0530

    init

---
+++ src/main/java/com/varun/QueuesPOC/QueuesPocApplication.java
@@ -0,0 +1,13 @@
+package com.varun.QueuesPOC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QueuesPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QueuesPocApplication.class, args);
+	}
+
+}
